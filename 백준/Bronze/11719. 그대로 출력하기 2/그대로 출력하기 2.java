54f300af647e7f92cc466799e7e2267e6ed63c77
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2025-01-14 11:29:26 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -2,17 +2,21 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 입력 초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        Boolean tf = true;
-        while(tf) {
+        while(true) {
+            // 문자열 입력
             String s = br.readLine();
+            
+            // 입력이 끝났을 시 종료, 안끝났으면 출력
             if (s == null) {
-                tf = false;
+                break;
             } else {
                 System.out.println(s);
             }
         }
+        // 입력 닫기
         br.close();
     }
 }

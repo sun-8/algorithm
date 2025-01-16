0e127fe56b78b73b5818640c5069500d9a978bd9
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2025-01-17 02:15:47 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -3,28 +3,17 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         
-        // 두 개의 정수 입력
-        String[] sArr = br.readLine().split(" ");
-        int n = Integer.parseInt(sArr[0]);
-        int m = Integer.parseInt(sArr[1]);
+        int n = Integer.parseInt(br.readLine().split(" ")[0]);
         
-        // n줄에 걸쳐 한 줄에 m개의 문자가 주어짐
         for(int i=0; i<n; i++) {
-            char[] cArr = br.readLine().toCharArray();
+            String s = br.readLine();
             
-            // 첫번째와 마지막 값을 바꾸기
-            for(int j=0; j<m/2; j++) {
-                char tmp = cArr[j];
-                cArr[j] = cArr[m-1-j];
-                cArr[m-1-j] = tmp;
-            }
-            bw.write(cArr);
-            bw.newLine();
+            sb.append(new StringBuilder(s).reverse()).append("\n");
+            
         }
-        bw.flush();
+        System.out.println(sb);
         br.close();
-        bw.close();
     }
 }

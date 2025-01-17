abad71ef35e37332f98590127f5eef5992879012
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2025-01-18 00:39:18 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -3,17 +3,17 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[][] sArr = new String[5][15];
+        String[] sArr = new String[5];
         StringBuilder sb = new StringBuilder();
         
         for(int i=0; i<sArr.length; i++) {
-            sArr[i] = br.readLine().split("");
+            sArr[i] = br.readLine();
         }
 
         for(int i=0; i<15; i++) {
             for(int j=0; j<5; j++) {
-                if (sArr[j].length > i) {
-                    sb.append(sArr[j][i]);
+                if (sArr[j].length() > i) {
+                    sb.append(sArr[j].charAt(i));
                 }
             }
         }

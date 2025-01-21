d80749c047acbc72e51cf2d744868987782c9831
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2025-01-22 01:19:21 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14272 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,9 +3,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        final int r = 31;
+        final int disjoint = 1234567891;
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int rtn = 0;
+        long squared = 1; // 제곱 계산 변수
+        long rtn = 0; // 결과 저장 변수
         
         // 문자열의 길이
         int l = Integer.parseInt(br.readLine());
@@ -16,7 +20,9 @@ public class Main {
         for(int i=0; i<l; i++) {
             // 소문자 a의 아스키 값은 97 (97-96=1)
             int charToInt = s.charAt(i)-96;
-            rtn += charToInt * Math.pow(31, i);
+            rtn = (rtn + (charToInt * squared) % disjoint) % disjoint;
+            // 다음 제곱값 계산
+            squared = (squared * r) % disjoint;
         }
         
         bw.write(String.valueOf(rtn));

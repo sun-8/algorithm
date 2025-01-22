2849947d2648fc1fa7bb5122fa7701a728920139
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2025-01-22 20:54:40 +0900

    [Bronze III] Title: 아 맞다 마늘, Time: 112 ms, Memory: 14588 KB -BaekjoonHub

---
+++ 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<String> hs = new HashSet<>();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        String[] ingredient = br.readLine().split(" ");
+        String[] realIngredient = br.readLine().split(" ");
+        for(int i=0; i<n-1; i++) {
+            hs.add(realIngredient[i]);
+        }
+        
+        for(int i=0; i<n; i++) {
+            if (!hs.contains(ingredient[i])) {
+                System.out.println(ingredient[i]);
+                br.close();
+                return;
+            }
+        }
+        
+        br.close();
+    }
+}

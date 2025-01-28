104a04eb76096301b08279516c48d05ba52befeb
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2025-01-28 18:46:48 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -9,26 +9,24 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
+        String[] sArr = br.readLine().split(" ");
+        int[] iArr = new int[n];
+        for (int i=0; i<n; i++) {
+            iArr[i] = Integer.parseInt(sArr[i]);
+        }
+        Arrays.sort(iArr);
+        
         // m을 넘지 않으면서 m에 최대한 가까워야 한다.
         // 세 숫자가 모두 달라야 한다.
-        String[] sArr = br.readLine().split(" ");
-        int one = 0, two = 0, three = 0, max = 0;
-        
+        int max = 0;
         for(int i=0; i<n-2; i++) {
-            // 첫번째 숫자
-            one = Integer.parseInt(sArr[i]);
-            
             for(int j=i+1; j<n-1; j++) {
-                // 두번째 숫자
-                two = Integer.parseInt(sArr[j]);
-                
                 for(int k=j+1; k<n; k++) {
-                    // 세번째 숫자
-                    three = Integer.parseInt(sArr[k]);
-                    if (max < one+two+three && one+two+three < m) {
-                        max = one+two+three;
-                    } else if (one+two+three == m) {
-                        System.out.println(one+two+three);
+                    if (max < iArr[i]+iArr[j]+iArr[k] 
+                        && iArr[i]+iArr[j]+iArr[k] < m) {
+                        max = iArr[i]+iArr[j]+iArr[k];
+                    } else if (iArr[i]+iArr[j]+iArr[k] == m) {
+                        System.out.println(iArr[i]+iArr[j]+iArr[k]);
                         return;
                     } 
                 }

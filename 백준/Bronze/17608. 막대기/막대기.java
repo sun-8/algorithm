9afad25296940b732c8b14d8ed0bf52178c1a6cd
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2025-02-05 08:50:53 +0900

    [Bronze II] Title: 막대기, Time: 228 ms, Memory: 25048 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,23 +1,29 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) {
-        int visibleCount = 0;
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < N; i++) {
-            int height = sc.nextInt();
-            // 스택이 비어있지 않고 현재 막대기의 높이가 스택의 맨 위 막대기보다 크다면
-            while (!stack.isEmpty() && height >= stack.peek()) {
-                stack.pop(); // 스택에서 맨 위의 막대기 제거
-            }
-            stack.push(height); // 현재 막대기를 스택에 추가
+        int n = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        
+        for(int i=0; i<n; i++) {
+            int m = Integer.parseInt(br.readLine());
+            stack.push(m);
         }
-
-        visibleCount = stack.size(); // 스택에 남아있는 막대기는 보이는 막대기들
-        System.out.println(visibleCount);
-        sc.close();
+        
+        int pop = stack.pop();
+        int cnt = 0;
+        for(int i=0; i<n-1; i++) {
+            if (pop < stack.peek()) {
+                cnt++;
+                pop = stack.pop();
+            } else if (pop >= stack.peek()) {
+                stack.pop();
+            }
+        }
+        System.out.println(cnt+1);
+        br.close();
     }
 }

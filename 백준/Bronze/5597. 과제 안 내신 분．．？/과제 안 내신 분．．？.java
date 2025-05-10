3638c0784b300021bddc4f10f009604c8fa35fa8
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2025-05-11 01:16:46 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -2,30 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    // 실행부부
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         
-        final int STUDENT_COUNT = 30;
-        final int SUBMIT_ASSIGNMENT = 28;
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        // 제출자 입력
-        for(int i=0; i<SUBMIT_ASSIGNMENT; i++) {
-            int count = Integer.parseInt(br.readLine());
-            map.put(count, count);
-        }
-        // 미제출자 탐색
-        for(int i=1; i<=STUDENT_COUNT; i++) {
-            if (map.get(i) == null) {
-                sb.append(i).append("\n");
-            }
-        }
-        
-        br.close();
-        bw.write(String.valueOf(sb));
+        Service service = new Service();
+        String result = service.findNonSubmitStudent(30, 28);
+
+        bw.write(result);
         bw.flush();
         bw.close();
     }
+}
+
+// 과제 미제출자를 찾는 프로그램
+class Service {
+    StudentManager studentManager;
+
+    public String findNonSubmitStudent(int studentCount, int submitCount) {
+        studentManager = new StudentManager(studentCount, submitCount);
+        Set<Integer> submitSet = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            // 제출자 입력
+            for (int i=0; i<studentManager.getSubmitCount(); i++) {
+                submitSet.add(Integer.parseInt(br.readLine()));
+            }
+            // 미제출자 탐색
+            for (int i=1; i<=studentManager.getStudentCount(); i++) {
+                if (!submitSet.contains(i)) {
+                    sb.append(i).append("\n");
+                }
+            }
+            studentManager.setNonSubmitStudent(String.valueOf(sb));
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        return studentManager.getNonSubmitStudent();
+    }
+
+}
+
+// 학생 관리 (학생 수, 과제 제출자 수, 미제출자)
+class StudentManager {
+    private int studentCount;
+    private int submitCount;
+    private String nonSubmitStudent;
+
+    public StudentManager() {}
+    public StudentManager(int studentCount, int submitCount) {
+        this.studentCount = studentCount;
+        this.submitCount = submitCount;
+    }
+
+    public void studentCount(int studentCount) {
+        this.studentCount = studentCount;
+    }
+    public int getStudentCount() {
+        return studentCount;
+    }
+    public void setSubmitCount(int submitCount) {
+        this.submitCount = submitCount;
+    }
+    public int getSubmitCount() {
+        return submitCount;
+    }
+    public void setNonSubmitStudent(String nonSubmitStudent) {
+        this.nonSubmitStudent = nonSubmitStudent;
+    }
+    public String getNonSubmitStudent() {
+        return nonSubmitStudent;
+    }
+
 }

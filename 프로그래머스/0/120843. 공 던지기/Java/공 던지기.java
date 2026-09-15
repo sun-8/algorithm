c1d4fb02c194eb15eaf844958ccfc536a3ecaebc
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2026-09-15 18:25:01 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/Java/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/Java/공 던지기.java
@@ -1,11 +1,7 @@
 class Solution {
     public int solution(int[] numbers, int k) {
         int answer = 0;
-        if (2 * (k-1) > numbers.length) {
-            answer = numbers[2 * (k-1) % numbers.length];
-        } else {
-            answer = numbers[2 * (k-1)];
-        }
+        answer = numbers[2 * (k-1) % numbers.length];
         return answer;
     }
 }

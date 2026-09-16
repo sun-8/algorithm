a955922e48d4716731897c417153f7abbfa72dd9
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2026-09-16 11:06:36 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/Java/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/Java/팩토리얼.java
@@ -1,16 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int i=1;
         int factorial = 1;
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=i; j++) {
-                factorial *= j;
+        while(true) {
+            factorial *= i;
+            if(factorial > n) {
+                answer = i-1;
+                break;
             }
-            if(factorial > n) break;
-            else {
-                answer = i;
-                factorial = 1;
-            }
+            else i++;
         }
         return answer;
     }

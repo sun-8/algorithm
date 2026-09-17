beb5397920ea75cdc1588b621646ddac3cc5728f
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2026-09-17 10:30:49 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 3.01 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/Java/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/Java/문자열 정렬하기 （1）.java
@@ -2,15 +2,11 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        String num_string = "";
-        for(int i=0; i<my_string.length(); i++) {
-            if(!((int)my_string.charAt(i) >= 97 && (int)my_string.charAt(i) <= 123)) {
-                num_string += my_string.charAt(i);
-            }
-        }
+        String num_string = my_string.replaceAll("[a-z]", "");
+        
         int[] answer = new int[num_string.length()];
         for(int i=0; i<num_string.length(); i++) {
-            answer[i] = Integer.parseInt(String.valueOf(num_string.charAt(i)));
+            answer[i] = num_string.charAt(i) - '0';
         }
         Arrays.sort(answer);
         return answer;

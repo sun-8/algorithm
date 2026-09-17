68b37354a25b6290ef0f377b020de032adb00ef5
HeeSun Jung <82883418+sun-8@users.noreply.github.com>
2026-09-17 10:15:58 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/Java/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/Java/모음 제거.java
@@ -1,11 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        answer = my_string.replaceAll("a", "")
-                            .replaceAll("e", "")
-                            .replaceAll("i", "")
-                            .replaceAll("o", "")
-                            .replaceAll("u", "");
+        answer = my_string.replaceAll("[aeiou]", "");
         return answer;
     }
 }
